39d240d5e23dca8a2d95851ede73cbd61cfe022b
gianluigi7 <gianluigiferraioli7@gmail.com>
2024-05-13 20:27:19 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+'use strict'
+
+
+let count;
+
+
+setTimeout(function () {
+   for (count = 10; count > 0; count--) {
+     console.log(count)
+
+   }
+
+});
+
+
+setTimeout(function ()  {
+   alert("Buon anno!");
+
+}, 10000);
