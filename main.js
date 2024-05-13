32b524dbbd742f93ae9a39907c6983f19d288d07
gianluigi7 <gianluigiferraioli7@gmail.com>
2024-05-13 20:58:51 +0200

    countdown

--- main.js
+++ main.js
@@ -2,18 +2,28 @@
 
 
 let count;
+let seconds = 9;
+const countdownEl = document.getElementById('countdown');
 
 
-setTimeout(function () {
-   for (count = 10; count > 0; count--) {
-     console.log(count)
+function countdown() {
+    countdownEl.innerHTML = `Mancano ${seconds} secondi`;
+    
+    
+    
+    if (seconds === 0) {
+        countdownEl.innerHTML = "Buon anno!";
+        return;
+    }
+        
+    
+    seconds--;
+}
 
-   }
-
-});
+setInterval(countdown, 1000);
 
 
-setTimeout(function ()  {
-   alert("Buon anno!");
-
-}, 10000);
+//setTimeout(function ()  {
+//   alert("Buon anno!");
+//
+//}, 10000);
